fa622a8338f677ae76c837e06eb88c7375428194
Daniil <dan-trofimov@mail.ru>
2020-01-22 17:18:23 +0300

    fixes

--- src/show_usage.c
+++ src/show_usage.c
@@ -6,7 +6,7 @@
 /*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 21:28:23 by bkayleen          #+#    #+#             */
-/*   Updated: 2019/12/07 16:04:08 by bkayleen         ###   ########.fr       */
+/*   Updated: 2020/01/22 17:16:39 by bkayleen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	spawn_error(char *name, char *error_msg)
 {
-	ft_printf("%s: %s\n", name, error_msg);
-	ft_printf("usage: ./ft_ls [-%s] [file ...]\n", AVAILABLE_FLAGS);
+	ft_errprintf("%s: %s\n", name, error_msg);
+	ft_errprintf("usage: ./ft_ls [-%s] [file ...]\n", AVAILABLE_FLAGS);
 }
